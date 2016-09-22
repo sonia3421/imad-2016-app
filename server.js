9323058c9431fd2be8f850d293f28c42d6098e37
sonia3421 <slrpandey2012@gmail.com>
2016-09-22 16:46:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,15 +6,18 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+    res.sendFile(path.join(_dirname, 'ui', 'index.html'));
 });
-app.get('article1'), function (req, res){
-    res.sendfile(path.join(_dirname, 'ui', 'index.html'));
-}
-app.get('article2'), function (req, res)
+
+app.get('/article1', function (req, res){
     res.sendfile(path.join(_dirname, 'ui', 'index.html'));
 });
-app.get('article3'), function (req, res)
+
+app.get('/article2', function (req, res){
+    res.sendfile(path.join(_dirname, 'ui', 'index.html'));
+});
+
+app.get('/article3', function (req, res){
     res.sendfile(path.join(_dirname, 'ui', 'index.html'));
 });
 
