a9f2e5f8912520613ffa24ef0e846d7ac4e16987
sonia3421 <slrpandey2012@gmail.com>
2016-09-22 16:55:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(_dirname, 'ui', 'index.html'));
 });
 
-app.get('/article1', function (req, res){
-    res.sendfile(path.join(_dirname, 'ui', 'index.html'));
+app.get('/article one', function (req, res){
+    res.send('article one requested and will be served here');
 });
 
-app.get('/article2', function (req, res){
-    res.sendfile(path.join(_dirname, 'ui', 'index.html'));
+app.get('/article two', function (req, res){
+    res.send('article two requested and will be served here');
 });
 
-app.get('/article3', function (req, res){
-    res.sendfile(path.join(_dirname, 'ui', 'index.html'));
+app.get('/article three', function (req, res){
+    res.send('article three requested and will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
