5281295c0e127d2d91ff26279adcdb4f36a18051
sonia3421 <slrpandey2012@gmail.com>
2016-09-23 15:37:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res){
-    res.sendFile(path.join(_dirname, 'ui', 'html'.
+    res.sendFile(path.join(_dirname, 'ui', 'html'));
 });
 
 app.get('/article-two', function (req, res){
